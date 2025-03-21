a26f8b13980e51178ceab202a818509ce59546c8
Erastus <emauliate17@gmail.com>
2025-03-21 11:23:46 +0700

    refactor: UserModel.js

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,28 +1,27 @@
-const pool = require('../config/db');
+const pool = require("../config/db");
 
 const UserModel = {
-    async createUser({ username, email, password, role }) {
-        const query = `
-      INSERT INTO users (username, email, password_hash, role)
+  async createUser({ username, email, password, role }) {
+    const query = `INSERT INTO users (username, email, password_hash, role)
       VALUES ($1, $2, $3, $4)
-      RETURNING *;
-    `;
-        const values = [username, email, password, role];
-        const { rows } = await pool.query(query, values);
-        return rows[0];
-    },
+      RETURNING *`;
 
-    async getUserByEmail(email) {
-        const query = `SELECT * FROM users WHERE email = $1;`;
-        const { rows } = await pool.query(query, [email]);
-        return rows[0] || null;
-    },
+    const values = [username, email, password, role];
+    const { rows } = await pool.query(query, values);
+    return rows[0];
+  },
 
-    async getUserByUsername(username) {
-        const query = `SELECT * FROM users WHERE username = $1;`;
-        const { rows } = await pool.query(query, [username]);
-        return rows[0] || null;
-    }
+  async getUserByEmail(email) {
+    const query = `SELECT * FROM users WHERE email = $1;`;
+    const { rows } = await pool.query(query, [email]);
+    return rows[0] || null;
+  },
+
+  async getUserByUsername(username) {
+    const query = `SELECT * FROM users WHERE username = $1;`;
+    const { rows } = await pool.query(query, [username]);
+    return rows[0] || null;
+  },
 };
 
 module.exports = UserModel;
