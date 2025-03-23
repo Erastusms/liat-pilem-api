b78a0d4453c91c2802dd2d179655e31f56b9260a
Erastus <emauliate17@gmail.com>
2025-03-23 19:47:01 +0700

    add route /logout in authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const AuthController = require("../controllers/authController");
+const AuthController = require("../controllers/AuthController");
 const authMiddleware = require("../middlewares/authMiddleware");
 const authRoute = express.Router();
 
 authRoute.post("/register", AuthController.register);
 authRoute.post("/login", AuthController.login);
+authRoute.post("/logout", AuthController.logout);
 authRoute.get("/profile", authMiddleware, AuthController.profile);
 authRoute.put("/profile", authMiddleware, AuthController.updateProfile);
 
